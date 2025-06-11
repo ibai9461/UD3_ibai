3926529d8cdb126a3ede1720198913f4f89a201d
Ibai8520 <igutierrezs10@educantabria.es>
2025-06-11 16:29:46 +0200

    a

---
+++ ED/src/main/java/com/mycompany/ed/ED.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ed;
+
+/**
+ *
+ * @author DAW120
+ */
+public class ED {
+
+    public static void main(String[] args) {
+        System.out.println("HOLAA");
+        System.out.println("HOLA");
+        
+    }
+}
